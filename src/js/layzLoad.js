daf227ffb3c1b000c6de66a4ff4e96fc68cce22a
rutenis <rutis98@gmail.com>
2019-10-08 13:59:57 +0100

    remove libraries, use observer instead

--- src/js/layzLoad.js
+++ src/js/layzLoad.js
@@ -1,8 +1,24 @@
-import LazyLoad from 'vanilla-lazyload';
-
 export default function lazyLoad() {
 
-  const lazyLoadInstance = new LazyLoad({
-    elements_selector: '.ln-lazy'
-  });
+  const targets = document.querySelectorAll('img');
+
+  const lazyLoad = target => {
+    const io = new IntersectionObserver((entries, observer) => {
+
+      entries.forEach(entry => {        
+
+        if (entry.isIntersecting) {
+          const img = entry.target;
+          const src = img.getAttribute('data-src');
+
+          img.setAttribute('src', src);          
+          observer.disconnect();
+        }
+      });
+    });
+
+    io.observe(target)
+  };
+
+  targets.forEach(lazyLoad);
 }
